7627d7e2b801778063ffb69c2c6658930eefd78f
Alexandru (The) Serban <alexandru@theserban.com>
2024-03-16 19:30:54 +0200

    First Deploy
    Making the image upload working. Still didnt figure out the database

---
+++ JS/db.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-analytics.js";
+import { getDatabase, set, get, update, remove, ref, child } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCn9OwLaTiuMBQg5VcBqIZe1TuU_J_yZh0",
+  authDomain: "submit-cronies.firebaseapp.com",
+  databaseURL: "https://submit-cronies-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "submit-cronies",
+  storageBucket: "submit-cronies.appspot.com",
+  messagingSenderId: "6576848992",
+  appId: "1:6576848992:web:afcabfac033ee76fec4305",
+  measurementId: "G-ZPLQ97XYTK"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getDatabase(app);
