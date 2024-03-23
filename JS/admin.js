3c1f89e19d5683f6ed4395f43e600e65c6c08198
Alexandru (The) Serban <alexandru@theserban.com>
2024-03-23 22:20:13 +0200

    Google

--- JS/admin.js
+++ JS/admin.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-analytics.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-database.js";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
 
 // Firebase configuration
 const firebaseConfig = {
@@ -33,32 +33,29 @@ function googleSignIn() {
     signInWithPopup(auth, provider)
     .then((result) => {
         // Handle the signed-in user information
-        // ...
+        console.log('Sign-in successful.');
     }).catch((error) => {
         // Handle errors here
-        // ...
+        console.error('SignIn error', error);
     });
 }
 
-// Function to initialize the theme switcher
-function initializeThemeSwitcher() {
-    const savedTheme = localStorage.getItem('theme') || 'light';
-    document.documentElement.setAttribute('data-theme', savedTheme);
-    const themeToggle = document.getElementById('themeToggle');
-    themeToggle.checked = savedTheme === 'dark';
-    themeToggle.addEventListener('click', () => {
-        const currentTheme = document.documentElement.getAttribute('data-theme');
-        const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
-        document.documentElement.setAttribute('data-theme', newTheme);
-        localStorage.setItem('theme', newTheme);
-    });
-}
-
-// DOMContentLoaded listener to initialize functionalities
+// Auth state change listener to toggle .voteUp, .voteDown, .uploadButton visibility
 document.addEventListener('DOMContentLoaded', () => {
-    initializeThemeSwitcher();
+    onAuthStateChanged(auth, (user) => {
+        const actionElements = document.querySelectorAll('.voteUp, .voteDown, .uploadButton');
+      
+        if (user) {
+            // User is signed in, show the buttons
+            actionElements.forEach(element => element.style.display = '');
+        } else {
+            // No user is signed in, keep the buttons hidden
+            actionElements.forEach(element => element.style.display = 'none');
+        }
+    });
+
     const googleLoginButton = document.getElementById('google-login');
-    if (googleLoginButton) { // Ensure button exists before adding listener
+    if (googleLoginButton) {
         googleLoginButton.addEventListener('click', googleSignIn);
     }
 });
